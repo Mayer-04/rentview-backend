23e47b076671aa31b0026460b36691f98f5da751
Mayer-04 <andres.20.chaves@gmail.com>
2025-11-24 01:45:16 -0500

    feat: agregar inserción de records

---
+++ db_scripts/02_records.sql
@@ -0,0 +1,233 @@
+INSERT INTO
+    records (
+        address,
+        country,
+        city,
+        housing_type,
+        monthly_rent
+    )
+VALUES
+    (
+        'Cra 45 #12-33, Apt 301',
+        'Colombia',
+        'Bogotá',
+        'apartamento',
+        2300000
+    ),
+    (
+        'Calle 10 #22-14',
+        'Colombia',
+        'Medellín',
+        'casa',
+        3200000
+    ),
+    (
+        'Av. 6N #34-21, Torre B Apt 804',
+        'Colombia',
+        'Cali',
+        'apartamento',
+        1800000
+    ),
+    (
+        'Cra 50 #98-20',
+        'Colombia',
+        'Barranquilla',
+        'comercial',
+        4500000
+    ),
+    (
+        'Calle 27 #18-55',
+        'Colombia',
+        'Bucaramanga',
+        'apartamento',
+        1500000
+    ),
+    (
+        'Transv 7 #115-44, Apt 702',
+        'Colombia',
+        'Bogotá',
+        'apartamento',
+        1600000
+    ),
+    (
+        'Cra 65 #42-17',
+        'Colombia',
+        'Medellín',
+        'casa',
+        650000
+    ),
+    (
+        'Cl 5 #42A-09',
+        'Colombia',
+        'Cali',
+        'casa',
+        2100000
+    ),
+    (
+        'Cra 21 #45-11',
+        'Colombia',
+        'Barranquilla',
+        'apartamento',
+        1400000
+    ),
+    (
+        'Cl 10 #7-60',
+        'Colombia',
+        'Bucaramanga',
+        'casa',
+        2600000
+    ),
+    (
+        'Cra 72 #30-19, Local 205',
+        'Colombia',
+        'Bogotá',
+        'comercial',
+        5200000
+    ),
+    (
+        'Cl 52 #45-78',
+        'Colombia',
+        'Medellín',
+        'apartamento',
+        1200000
+    ),
+    (
+        'Av 4N #33-19',
+        'Colombia',
+        'Cali',
+        'casa',
+        3500000
+    ),
+    (
+        'Cl 91 #65-40',
+        'Colombia',
+        'Barranquilla',
+        'apartamento',
+        2300000
+    ),
+    (
+        'Cra 14 #22-19, Apt 501',
+        'Colombia',
+        'Bucaramanga',
+        'apartamento',
+        1750000
+    ),
+    (
+        'Cl 99 #15-33',
+        'Colombia',
+        'Bogotá',
+        'comercial',
+        6800000
+    ),
+    (
+        'Cra 50 #18-21',
+        'Colombia',
+        'Medellín',
+        'casa',
+        2000000
+    ),
+    (
+        'Cl 33 #11-88',
+        'Colombia',
+        'Cali',
+        'apartamento',
+        1550000
+    ),
+    (
+        'Cra 12 #44-09',
+        'Colombia',
+        'Barranquilla',
+        'comercial',
+        4300000
+    ),
+    (
+        'Cl 4 #21-78',
+        'Colombia',
+        'Bucaramanga',
+        'comercial',
+        3900000
+    );
+
+INSERT INTO
+    record_images (record_id, image_url)
+VALUES
+    (
+        1,
+        'https://cdn.pixabay.com/photo/2015/10/20/18/57/furniture-998265_1280.jpg'
+    ),
+    (
+        2,
+        'https://cdn.pixabay.com/photo/2017/07/09/03/19/home-2486092_1280.jpg'
+    ),
+    (
+        3,
+        'https://cdn.pixabay.com/photo/2016/11/23/14/29/living-room-1853203_1280.jpg'
+    ),
+    (
+        4,
+        'https://cdn.pixabay.com/photo/2017/03/28/12/10/chairs-2181947_1280.jpg'
+    ),
+    (
+        5,
+        'https://cdn.pixabay.com/photo/2017/03/25/23/32/kitchen-2174593_1280.jpg'
+    ),
+    (
+        6,
+        'https://cdn.pixabay.com/photo/2017/01/07/17/48/interior-1961070_1280.jpg'
+    ),
+    (
+        7,
+        'https://cdn.pixabay.com/photo/2016/11/22/19/11/brick-wall-1850095_1280.jpg'
+    ),
+    (
+        8,
+        'https://cdn.pixabay.com/photo/2018/03/01/18/17/house-3191353_1280.jpg'
+    ),
+    (
+        9,
+        'https://cdn.pixabay.com/photo/2019/05/22/07/53/old-home-4220856_1280.jpg'
+    ),
+    (
+        10,
+        'https://cdn.pixabay.com/photo/2023/10/04/16/22/house-8294010_1280.jpg'
+    ),
+    (
+        11,
+        'https://cdn.pixabay.com/photo/2014/10/05/22/39/real-estate-475875_1280.jpg'
+    ),
+    (
+        12,
+        'https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg'
+    ),
+    (
+        13,
+        'https://cdn.pixabay.com/photo/2015/08/04/18/50/mansion-875094_1280.jpg'
+    ),
+    (
+        14,
+        'https://cdn.pixabay.com/photo/2018/03/18/15/26/villa-3237114_1280.jpg'
+    ),
+    (
+        15,
+        'https://cdn.pixabay.com/photo/2019/01/27/13/12/house-3958071_1280.jpg'
+    ),
+    (
+        16,
+        'https://cdn.pixabay.com/photo/2015/03/09/15/55/hotel-665929_1280.jpg'
+    ),
+    (
+        17,
+        'https://cdn.pixabay.com/photo/2021/10/01/19/17/iceland-6673451_1280.jpg'
+    ),
+    (
+        18,
+        'https://cdn.pixabay.com/photo/2020/02/08/16/14/villa-4830490_1280.jpg'
+    ),
+    (
+        19,
+        'https://cdn.pixabay.com/photo/2022/11/07/18/13/haunted-house-7576968_1280.jpg'
+    ),
+    (
+        20,
+        'https://cdn.pixabay.com/photo/2019/12/31/16/10/lost-places-4732238_1280.jpg'
+    );
